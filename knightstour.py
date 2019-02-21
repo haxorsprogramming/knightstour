711df3989e84a8f683afdd16a53708a29e19f812
Boggy <hypatia@Yours-MacBook-Air.local>
2019-02-21 12:11:48 -0500

    adding files to repository. graphics.py is not my code

---
+++ knightstour.py
@@ -0,0 +1,10 @@
+'''
+knightstour.py
+
+Finds a "knight's tour" of an n x n chessboard. A knight's tour is a
+sequence of moves that a knight can take where the knight only visits
+each square once. Mind that a knight moves in an "L" shape across the 
+board. from any point. that is, a knight moves three spaces along either
+the x and y
+
+'''
